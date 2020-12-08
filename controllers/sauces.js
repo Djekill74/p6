d1be9ef5010ad0e9c4ab8a919c075e8187bd7f28
Djekill74 <jerome.delagneau@orange.fr>
2020-12-08 01:13:51 +0100

    get

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -2,7 +2,7 @@ const Sauce = require('../models/Sauce');
 
 exports.createSauce = (req, res, next) => {
    delete req.body._id;
-   console.warn('body' + req.body.name);
+   console.warn('body' + req.body);
    const sauce = new Sauce({
         name: req.body.name
     });
@@ -10,3 +10,21 @@ exports.createSauce = (req, res, next) => {
     .then(() => res.status(201).json({ message: 'suace enregistré'}))
     .catch(error => res.status(400).json({ error }));
 };
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({
+        _id: req.params.id
+    })
+    .then(
+        (thing) => {res.status(200).json(thing);}
+    )
+    .catch(error => res.status(404).json({ error: error }));
+};
+
+exports.getAllSauce = (req, res, next) => {
+    Sauce.find()
+    .then(
+        (sauces) => {res.status(200).json(sauces);}
+    )
+    .catch(error => res.status(400).json({ error }));
+}
--- routes/sauces.js
+++ routes/sauces.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const saucesCtrl = require('../controllers/sauces');
 const auth = require('../middleware/auth');
 
+router.get('/', auth, saucesCtrl.getAllSauce);
 router.post('/', auth, saucesCtrl.createSauce);
+router.get('/:id', saucesCtrl.getOneSauce);
 
 module.exports = router;
